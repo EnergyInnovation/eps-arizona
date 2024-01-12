a30c21a428b1cbbcd91900ffe500cc10ef88c917
oashmoore <olivia@energyinnovation.org>
2024-01-11 18:57:02 -0800

    4.0 files

--- GeneratedDataLoggingScript.cmd
+++ GeneratedDataLoggingScript.cmd
@@ -4,7 +4,13 @@ SIMULATE>SAVELIST|OutputVarsToExport.lst
 SIMULATE>RUNNAME|NoSettings
 SIMULATE>READCIN|
 MENU>RUN|O
-MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport.lst|+!||2020|2050|:NoSettings
+MENU>VDF2TAB|NoSettings.vdfx|NoSettings.tsv|OutputVarsToExport.lst|+!||2021|2050|:NoSettings
 FILE>DELETE|NoSettings.vdfx
+
+SIMULATE>RUNNAME|100ces
+SIMULATE>READCIN|100ces.cin
+MENU>RUN|O
+MENU>VDF2TAB|100ces.vdfx|100ces.tsv|OutputVarsToExport.lst|+!||2021|2050|:100ces
+FILE>DELETE|100ces.vdfx
 
 SIMULATE>SAVELIST|
